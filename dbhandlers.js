c24aecd745fcca7698d287b431efaf049e011900
Max Haubenstock <max.haub@gmail.com>
2012-02-20 23:00:05 -0600

    Added in error callbacks

--- dbhandlers.js
+++ dbhandlers.js
@@ -68,14 +68,20 @@ UserProvider.prototype.findAll = function (callback) {
 
 UserProvider.prototype.findUser = function (params, callback) {
     User.findOne(params, function (err, user) {
-        callback(null, user);
+  			if(err)
+  				callback(err, null)
+  			else 
+       	 callback(null, user);
     });
 }
 
 UserProvider.prototype.findById = function (id, callback) {
     User.findById(id, function (err, user) {
         //user is a {user}
-        callback(null, user);
+        if(err)
+        	callback(err, null);
+        else
+        	callback(null, user);
     });
 };
 
